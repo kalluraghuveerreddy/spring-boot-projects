1f9f331f37547f2fb351d0a0987ad2a12c7f5f87
Kallu Raghuveer Reddy <kalluraghureddy@gmail.com>
2024-03-21 22:18:04 +0530

    Added Spring Boot Security Database Authentication

---
+++ spring-boot-blogger-app/src/main/java/com/practice/bloggerapp/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.practice.bloggerapp.repository;
+
+import com.practice.bloggerapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
